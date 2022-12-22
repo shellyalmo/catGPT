c4a986754f732f56431c5795032fd7d51e4ad9ea
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2022-12-22 14:07:02 +0200

    initial commit

---
+++ chat.js
@@ -0,0 +1,18 @@
+let replies = ["hello","thanks","ok dude","whatever","that's cool","not sure","I don't know","bye"]
+
+const replyRandomAnswer = (repliesArr) => {
+    let randomReply = repliesArr[Math.floor(Math.random() * repliesArr.length)]
+    return randomReply
+}
+
+const chatInputHandler = () => {
+    event.preventDefault();
+  const chatInput = document.getElementById("user-input");
+    let answer = replyRandomAnswer(replies)
+    console.log("it works")
+    console.log(answer)
+    document.getElementById("input-display").innerHTML+= `<br>` + (chatInput.value);
+    document.getElementById("input-display").innerHTML+= `<br>` + (answer);
+    document.getElementById("chatForm").reset()
+  
+};
