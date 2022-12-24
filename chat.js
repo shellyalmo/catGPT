16642847a04b39d9a2e6e49b14a474c1a92b36ed
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2022-12-24 22:18:17 +0200

    fix: submit input works

--- chat.js
+++ chat.js
@@ -29,11 +29,21 @@ const respondAsCat = () => {
 const chatInputHandler = () => {
   event.preventDefault();
   const chatInput = document.getElementById("user-input");
-  let answer = replyRandomAnswer(replies);
+  // let answer = replyRandomAnswer(replies);
   console.log("it works");
-  console.log(answer);
-  document.getElementById("input-display").innerHTML +=
-    `<br>` + chatInput.value;
-  document.getElementById("input-display").innerHTML += `<br>` + answer;
+  // console.log(answer);
+  document.getElementById("input-display").innerHTML += `<div >
+          <div>
+            <p id="input-display">
+              <img class="userAvatar" src="./assets/user_avatar.png" /> ${chatInput.value}
+            </p>
+          </div>
+          <div class="cat-response">
+            <img src="./assets/catGPT_icon.png" />
+            <img class="catImageResponse" src="https://cataas.com/cat" alt="" />
+          </div>
+        </div>`
+  // document.getElementById("input-display").innerHTML += `<br>` + answer;
+
   document.getElementById("chatForm").reset();
 };
