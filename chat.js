112e52919d400e56477f111c43f88e48fa5869c1
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2022-12-24 13:14:06 +0200

    feat: move cat gif to the chat

--- chat.js
+++ chat.js
@@ -1,31 +1,39 @@
-let replies = ["hello","thanks","ok dude","whatever","that's cool","not sure","I don't know","bye"]
+let replies = [
+  "meow",
+  "מיאו",
+  "meooow",
+  "meow-meow",
+  "meow!!!",
+  "meow?"
+];
 
 const replyRandomAnswer = (repliesArr) => {
-    let randomReply = repliesArr[Math.floor(Math.random() * repliesArr.length)]
-    return randomReply
-}
+  let randomReply = repliesArr[Math.floor(Math.random() * repliesArr.length)];
+  return randomReply;
+};
 
-const clearConversation = () =>{
-    location.reload();
-}
+const clearConversation = () => {
+  location.reload();
+};
 
 const respondAsCat = () => {
-    fetch("https://cataas.com/cat")
+  fetch("https://cataas.com/cat")
     .then((Response) => Response.blob())
-    .then(catBlob=>{
-
-        document.getElementById("cat-response")?.setAttribute("src",URL.createObjectURL(catBlob))
-    })
-}
+    .then((catBlob) => {
+      document
+        .getElementById("cat-response")
+        ?.setAttribute("src", URL.createObjectURL(catBlob));
+    });
+};
 
 const chatInputHandler = () => {
-    event.preventDefault();
+  event.preventDefault();
   const chatInput = document.getElementById("user-input");
-    let answer = replyRandomAnswer(replies)
-    console.log("it works")
-    console.log(answer)
-    document.getElementById("input-display").innerHTML+= `<br>` + (chatInput.value);
-    document.getElementById("input-display").innerHTML+= `<br>` + (answer);
-    document.getElementById("chatForm").reset()
-  
+  let answer = replyRandomAnswer(replies);
+  console.log("it works");
+  console.log(answer);
+  document.getElementById("input-display").innerHTML +=
+    `<br>` + chatInput.value;
+  document.getElementById("input-display").innerHTML += `<br>` + answer;
+  document.getElementById("chatForm").reset();
 };
